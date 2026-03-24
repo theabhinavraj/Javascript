45eaf02e2d956d3f815e1acf15463b91e9195592
Abhinav Raj <111248932+theabhinavraj@users.noreply.github.com>
2026-03-24 12:35:57 +0000

    3rd commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,47 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN (Not a Number)
 // true => 1; false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //1 => true ; 0 => false
 // " " => false
 // "Abhinav" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber); 
+
+
+// ************************. Operations ************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 = "Abhinav"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(+true);
+
+
+let gameCounter = 100
+++gameCounter;
+
+console.log(gameCounter);
