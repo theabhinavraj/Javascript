e8abe0c40e7f43bce86d164ca21c28a158c504be
Abhinav Raj <111248932+theabhinavraj@users.noreply.github.com>
2026-03-24 11:29:10 +0000

    Initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144554
+let accountEmail = "abhinav@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+accountEmail = "ab@ab.com"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountState);
+/* 
+prefer not to use var 
+because of issue in block scope and functional scope 
+
+*/
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3 + 3) 
+
+console.log("Abhinav")
+
+let name = "Abhinav"
+let age = 21
+let isLoggedIn = true
+ 
